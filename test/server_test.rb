9ca96b1bfd56569d9a1ff62c26dd25d70167a77f
Marcella Wigg <marcellakathrynwigg@gmail.com>
2016-04-11 13:55:49 -0600

    Adding client test, finishing first test

---
+++ test/server_test.rb
@@ -0,0 +1,12 @@
+require './lib/server'
+require 'minitest/autorun'
+require 'minitest/pride'
+
+class ServerTest < Minitest::Test
+
+  def test_it_listens_on_default_port
+    assert_equal 9292, Server.new.tcp_server
+  end
+
+
+end
